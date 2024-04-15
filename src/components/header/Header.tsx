ed9547c2b11996a6af79cf68d84737ffee7ed3bb
Suresh901 <sureshshrestha915@gmail.com>
2024-04-15 13:45:59 +0545

    Header changed

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+"use client"
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { IoMenu } from "react-icons/io5";
 
 const Header = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollPos, visible]);
+
   return (
-    <div className='z-[999] fixed top-0 w-full bg-transparent shadow-sm backdrop-blur-md'>
+    <div className={`z-[999] fixed top-0 w-full bg-transparent shadow-sm backdrop-blur-md transition-all duration-300 ${visible ? 'opacity-100' : 'opacity-0 translate-y-[-100%]'}`}>
       <div className='flex items-center justify-between mx-5 lg:mx-10 py-5'>
         <div className='flex items-center gap-2 cursor-pointer'>
           <Image src='https://metalogic.com.np/metalogo.png' alt='image' width={30} height={30} className='w-10 h-10' />
@@ -13,11 +31,11 @@ const Header = () => {
         </div>
         <div className='lg:flex items-center gap-7 cursor-pointer font-semibold hidden text-md'>
           <Link href='/'>
-          <h1>Home</h1>
+            <h1>Home</h1>
           </Link>
           <div>Services</div>
           <Link href='careers'>
-          <h1>Careers</h1>
+            <h1>Careers</h1>
           </Link> 
           <div>Blogs</div>
           <div>About Us</div>
