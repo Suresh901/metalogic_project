30be838490d9833470bfe0446b0bf7c3485541b6
Suresh901 <sureshshrestha915@gmail.com>
2024-04-16 19:49:49 +0545

    routes added for all menu items

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+   <div className='bg-[#003385] h-screen  flex items-center justify-center'>
+      <h1 className='text-white text-3xl font-bold'>This is About Us page</h1>
+    </div>
+  )
+}
+
+export default page
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,10 +1,9 @@
-
 import React from 'react'
 
 const page = () => {
   return (
     <div className='bg-[#003385] h-screen  flex items-center justify-center'>
-      <h1 className='text-white text-3xl font-bold'>This is HomePage</h1>
+      <h1 className='text-white text-3xl font-bold'>This is Service Page</h1>
     </div>
   )
 }
