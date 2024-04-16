65b0dba63685c3b46f8492f18ddc8b01112d6901
Suresh901 <sureshshrestha915@gmail.com>
2024-04-16 13:45:43 +0545

    i icon added

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { IoMenu } from "react-icons/io5";
+import { GiInfo } from "react-icons/gi";
 
 const Header = () => {
   const [prevScrollPos, setPrevScrollPos] = useState(0);
@@ -25,9 +26,10 @@ const Header = () => {
   return (
     <div className={`z-[999] fixed top-0 w-full bg-transparent shadow-sm backdrop-blur-md transition-all duration-300 ${visible ? 'opacity-100' : 'opacity-0 translate-y-[-100%]'}`}>
       <div className='flex items-center justify-between mx-5 lg:mx-10 py-5'>
-        <div className='flex items-center gap-2 cursor-pointer'>
+        <div className='flex items-center gap-2 cursor-pointer relative'>
           <Image src='https://metalogic.com.np/metalogo.png' alt='image' width={30} height={30} className='w-10 h-10' />
-          <h1 className='text-2xl lg:text-3xl font-semibold text-[#334767] '>MetaLogic</h1>
+          <h1 className='text-2xl lg:text-3xl font-semibold text-[#334767]'>MetaLogic</h1>
+          <GiInfo size={20} className='absolute top-[-10px] right-[-10px]'/>
         </div>
         <div className='lg:flex items-center gap-7 cursor-pointer font-semibold hidden text-md'>
           <Link href='/'>
